6878845e47f224bc46453232a2b0463a43e42407
Ben Sherman <3365+bensherman@users.noreply.github.com>
2021-06-04 19:09:12 -0700

    sort of works.

--- life2.rb
+++ life2.rb
@@ -1,5 +1,6 @@
 require "byebug"
 require "set"
+require "gosu"
 
 class Grid
   attr_accessor :cells
@@ -67,23 +68,78 @@ class Screen
   end
 end
 
-if __FILE__ == $0
-  height = 25
-  width = 80
-  grid = Grid.new
-  grid.add_cell(0, 0)
-  grid.add_cell(1, 0)
-  grid.add_cell(2, 0)
-  grid.add_cell(2, 10)
-  1000.times do
-    grid.add_cell(rand(-width/2..width/2), rand(-height/2..height/2))
-  end
+class Ascii
+  def self.ascii_screen
+    height = 25
+    width = 80
+    grid = Grid.new
+    grid.add_cell(0, 0)
+    grid.add_cell(1, 0)
+    grid.add_cell(2, 0)
+    grid.add_cell(2, 10)
+    1000.times do
+      grid.add_cell(rand(-width / 2..width / 2), rand(-height / 2..height / 2))
+    end
 
-  screen = Screen.new(width, height)
-  loop do
-    screen.draw grid.cells
-    puts(grid.cells.length)
-    grid = grid.next
-    sleep 0.2
+    screen = Screen.new(width, height)
+    loop do
+      screen.draw grid.cells
+      puts(grid.cells.length)
+      grid = grid.next
+      sleep 0.01
+    end
   end
 end
+
+class Game < Gosu::Window
+  def initialize
+    @pixel_size = 10
+    @height = 100
+    @width = 100
+    # @options = {update_interval: 500}
+    super @width * @pixel_size, @height * @pixel_size
+    self.caption = "Life"
+    @grid = Grid.new
+    @grid.add_cell(0, 0)
+    @grid.add_cell(1, 0)
+    @grid.add_cell(2, 0)
+    @grid.add_cell(2, 10)
+    1000.times do
+      @grid.add_cell(rand(@width), rand(@height))
+    end
+
+    @alive = record(@pixel_size, @pixel_size) do
+      draw_rect(0, 0, @pixel_size, @pixel_size, Gosu::Color::WHITE)
+    end
+
+    @dead = record(@pixel_size, @pixel_size) do
+      draw_rect(0, 0, @pixel_size, @pixel_size, Gosu::Color::BLACK)
+    end
+  ends
+  def update
+    @alive_cells = []
+    @dead_cells = []
+    @height.times do |y|
+      @width.times do |x|
+        if @grid.cells.include?({ x: x, y: y })
+          @alive_cells.append({ x: x, y: y })
+        else
+          @dead_cells.append({ x: x, y: y })
+        end
+      end
+    end
+    @grid = @grid.next
+  end
+
+  def draw
+    puts("#{@alive_cells.length} #{@dead_cells.length}")
+    @alive_cells.each do |c|
+      @alive.draw c[:x] * 10, c[:y] * 10
+    end
+    @dead_cells.each do |c|
+      @dead.draw c[:x] * 10, c[:y] * 10
+    end
+  end
+end
+
+Game.new.show if __FILE__ == $0
