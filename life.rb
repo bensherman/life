96fcad59723ab14b756edd4ab4a0769cadebaa56
Ben Sherman <3365+bensherman@users.noreply.github.com>
2021-06-06 17:03:58 -0700

    MVP

--- life.rb
+++ life.rb
@@ -1,177 +1,121 @@
-require "gosu"
 require "byebug"
-
-class Life < Gosu::Window
-  def initialize
-    super 1000, 1000
-    self.caption = "larf"
-    @cursor_x = 500
-    @cursor_y = 500
-  end
-
-  def draw
-  end
-end
-
-class Cell
-  def initialize(alive: false)
-    @alive = alive
-  end
-
-  def alive?
-    @alive
-  end
-
-  def die!
-    @alive = false
-  end
-end
-
-class Location
-  attr_reader :x, :y
-
-  def initialize(coords)
-    @x = coords[0]
-    @y = coords[1]
-  end
-
-  def hash
-    coords.hash
-  end
-
-  def eql?(other)
-    x == other.x && y == other.y
-  end
-
-  def coords
-    [x, y]
-  end
-
-  def n
-    y = @y + 1
-    x = @x
-    Location.new([x, y])
-  end
-
-  def s
-    y = @y - 1
-    x = @x
-    Location.new([x, y])
-  end
-
-  def e
-    y = @y
-    x = @x + 1
-    Location.new([x, y])
-  end
-
-  def w
-    y = @y
-    x = @x - 1
-    Location.new([x, y])
-  end
-
-  def nw
-    y = @y + 1
-    x = @x - 1
-    Location.new([x, y])
-  end
-
-  def ne
-    y = @y + 1
-    x = @x + 1
-    Location.new([x, y])
-  end
-
-  def sw
-    y = @y - 1
-    x = @x - 1
-    Location.new([x, y])
-  end
-
-  def se
-    y = @y - 1
-    x = @x + 1
-    Location.new([x, y])
-  end
-
-  def neighbors
-    [n, ne, e, se, s, sw, w, nw]
-  end
-end
+require "set"
+require "gosu"
+require "benchmark"
 
 class Grid
   attr_accessor :cells
-  def initialize(width, height)
-    @screen_w = width
-    @screen_h = height
-    @cells = {}
+
+  def initialize
+    @cells = Set[]
   end
 
-  def draw
-    (-@screen_h/2..@screen_h/2).each do |y|
-      (-@screen_w/2..@screen_w/2).each do |x|
-        c = cells[Location.new([x,y])]&.alive? ? "O" : "."
-        print(c)
-      end
-      puts("")
-      STDOUT.flush
-    end
-    puts("\n\n\n")
+  def add_cell(x, y)
+    @cells.add({ x: x, y: y })
   end
 
-  def add(x, y)
-    location = Location.new([x,y])
-    cells[location] = Cell.new(alive: true)
-    location.neighbors.each do |neighbor|
-      cells[neighbor] = cells.fetch(neighbor, Cell.new)
-    end
-  end
-
-  def deep_dup(obj)
-    Marshal.load(Marshal.dump(obj))
+  def neighbors(x, y)
+    Set[
+      { x: x + 1, y: y + 1 }, # northeast
+      { x: x - 1, y: y - 1 }, # southwest
+      { x: x + 1, y: y - 1 }, # southeast
+      { x: x - 1, y: y + 1 }, # northwest
+      { x: x + 1, y: y }, # east
+      { x: x, y: y + 1 }, # north
+      { x: x - 1, y: y }, # west
+      { x: x, y: y - 1 } # south
+    ]
   end
 
   def next
-    new_cells = deep_dup cells
+    new_cells = cells.dup
+    checklist = cells.dup
+    cells.each do |cell|
+      checklist += neighbors(cell[:x], cell[:y])
+    end
 
-    cells.each do |location, _cell|
-      alive_neighbors = 0
-      location.neighbors.each do |neighbor|
-        cells[neighbor]&.alive? && alive_neighbors += 1
-      end
-
-      if alive_neighbors.zero?
-        new_cells.delete location
-      elsif alive_neighbors == 1 || alive_neighbors > 3
-        new_cells[location]&.die!
-      elsif alive_neighbors == 2
-        next
-      elsif alive_neighbors == 3
-        new_cells[location] = Cell.new(alive: true)
-        location.neighbors.each do |neighbor|
-          new_cells[neighbor] = new_cells.fetch(neighbor, Cell.new(alive: false))
-        end
+    puts("checklist size: #{checklist.length}")
+    checklist.each do |cell|
+      live_neighbors = count_live_neighbors(cell[:x], cell[:y])
+      if live_neighbors < 2 || live_neighbors > 3
+        new_cells.delete cell
+      elsif live_neighbors == 3
+        new_cells.add({ x: cell[:x], y: cell[:y] })
       end
     end
     @cells = new_cells
   end
-end
 
-if __FILE__ == $0
-  height = 30
-  width = 120
-  grid = Grid.new(width, height)
-  grid.add(0, 0)
-  grid.add(1, 0)
-  grid.add(2, 0)
-  1000.times do
-    grid.add(rand(-width..width), rand(-height..height))
-  end
-
-  loop do
-    grid.draw
-    grid.next
-    sleep 0.3
+  def count_live_neighbors(x, y)
+    (@cells & neighbors(x, y)).length
   end
 end
+
+class Game < Gosu::Window
+  def initialize
+    @pixel_size = 10
+    @height = 100
+    @width = 100
+    super @width * @pixel_size, @height * @pixel_size, @options = { update_interval: 10 }
+    self.caption = "Life"
+    @grid = Grid.new
+    @grid.add_cell(0, 0)
+    @grid.add_cell(1, 0)
+    @grid.add_cell(2, 0)
+    @grid.add_cell(2, 10)
+    @alive_cells = []
+    1000.times do
+      @grid.add_cell(rand(@width), rand(@height))
+    end
+
+    @alive = record(@pixel_size, @pixel_size) do
+      draw_rect(0, 0, @pixel_size, @pixel_size, Gosu::Color::WHITE)
+    end
+  end
+
+  def update
+    if @adding
+      puts(mouse_x, mouse_y)
+      pixel_x = mouse_x.to_i / @pixel_size
+      pixel_y = mouse_y.to_i / @pixel_size
+      @grid.add_cell pixel_x, pixel_y
+      puts("adding cell at #{pixel_x} #{pixel_y}")
+      @alive_cells.append({ x: pixel_x, y: pixel_y })
+    end
+    unless @paused || @mouse_paused
+      @alive_cells = []
+      puts("cells: #{@grid.cells.length}")
+      @grid.cells.each do |c|
+        @alive_cells.append(c) if c[:x].positive? && c[:x] < @width && c[:y].positive? && c[:y] < @height
+      end
+      @grid.next
+    end
+  end
+
+  def draw
+    puts("alive pixels: #{@alive_cells.length}")
+    @alive_cells.each do |c|
+      @alive.draw(c[:x] * @pixel_size, c[:y] * @pixel_size)
+    end
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KB_SPACE
+      @paused = !@paused
+    when Gosu::MS_LEFT
+      @mouse_paused = true
+      @adding = true
+    end
+  end
+
+  def button_up(id)
+    case id
+    when Gosu::MS_LEFT
+      @adding = false
+      @mouse_paused = false
+    end
+  end
+end
+
+Game.new.show if __FILE__ == $0
